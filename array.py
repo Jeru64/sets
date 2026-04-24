a24f2b7acc1cae2a15b15006d72bb5f62db8f4f3
Jeru64 <cherish.jacob@gmail.com>
2026-04-24 17:07:18 +0400

    Add files via upload

---
+++ array.py
@@ -0,0 +1,6 @@
+import array as arr
+array_num=arr.array('i',[1,3,5,3,7,9,3])
+print("Original array:"+str(array_num))
+print("Count of 3 in the array:"+str(array_num.count(3)))
+array_num.reverse()
+print("Reversed array:"+str(array_num))
